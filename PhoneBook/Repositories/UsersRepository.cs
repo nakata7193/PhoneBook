6d32a3ffdf4fa633ce785161e655bbcd5b4c1dfc
nakata7193 <atanas925@gmail.com>
2022-01-29 00:27:07 +0200

    added users controller

---
+++ PhoneBook/Repositories/UsersRepository.cs
@@ -0,0 +1,52 @@
+﻿
+
+using PhoneBook.Entities;
+using System.Collections.Generic;
+
+namespace PhoneBook.Repositories
+{
+    public class UsersRepository
+    {
+        public static List<User> Items { get; set; }
+
+        static UsersRepository()
+        {
+            Items = new List<User>()
+            {
+                new User()
+                {
+                    ID = 1,
+                    Username = "sadawfga",
+                    Password = "password",
+                    FirstName = "Petar",
+                    LastName = "Valchanov",
+                },
+                new User()
+                {
+                    ID = 2,
+                    Username = "fdggfhjfgh",
+                    Password = "password",
+                    FirstName = "Gosho",
+                    LastName = "Ivanov",
+                },
+                new User()
+                {
+                    ID = 3,
+                    Username = "gdfgafgawes",
+                    Password = "password",
+                    FirstName = "Ivan",
+                    LastName = "Petkov",
+                },
+                new User()
+                {
+                    ID = 4,
+                    Username = "efshfdszer",
+                    Password = "password",
+                    FirstName = "Miro",
+                    LastName = "Dobrev",
+                }
+
+            };
+        }
+    }
+}
